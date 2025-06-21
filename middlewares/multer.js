0802e32cc4194f5af5be10e8a5afe513b1396ac3
Bugbuster2004 <siddhant6338bansal@gmail.com>
2025-06-21 13:46:27 +0530

    updated multyer.js

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,19 +1,19 @@
 // middleware/multer.js
-import multer from 'multer';
-import path from 'path';
-import { v4 as uuidv4 } from 'uuid';
+import multer from "multer";
+import path from "path";
+import { v4 as uuidv4 } from "uuid";
 
-// import fs from 'fs';
-// const uploadDir = path.join(process.cwd(), 'uploads');
+import fs from "fs";
+const uploadDir = path.join(process.cwd(), "uploads");
 
-// // Auto-create uploads folder if it doesn't exist
-// if (!fs.existsSync(uploadDir)) {
-//   fs.mkdirSync(uploadDir, { recursive: true });
-// }
+// Auto-create uploads folder if it doesn't exist
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // make sure 'uploads' folder exists
+    cb(null, "uploads/"); // make sure 'uploads' folder exists
   },
   filename: function (req, file, cb) {
     const ext = path.extname(file.originalname);
@@ -25,12 +25,14 @@ const upload = multer({
   storage,
   fileFilter: function (req, file, cb) {
     const allowedTypes = /jpeg|jpg|png/;
-    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+    const extname = allowedTypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
     const mimetype = allowedTypes.test(file.mimetype);
     if (mimetype && extname) {
       return cb(null, true);
     }
-    cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+    cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
   },
 });
 
